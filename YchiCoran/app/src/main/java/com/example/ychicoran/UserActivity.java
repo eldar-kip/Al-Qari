b1fcb1f2571c0ff7184617e886a3faac077f0bcc
eldar-kip <odzha.eldar.i.3.24@gmail.com>
2026-08-11 09:19:31 +0300

    Замена пользователя на настройки

--- YchiCoran/app/src/main/java/com/example/ychicoran/UserActivity.java
+++ YchiCoran/app/src/main/java/com/example/ychicoran/UserActivity.java
@@ -3,13 +3,11 @@ package com.example.ychicoran;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ychicoran.dopclasses.BaseActivity;
-import com.example.ychicoran.dopclasses.NavigationProject;
 
 public class UserActivity extends BaseActivity {
 
@@ -17,7 +15,7 @@ public class UserActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_settings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
