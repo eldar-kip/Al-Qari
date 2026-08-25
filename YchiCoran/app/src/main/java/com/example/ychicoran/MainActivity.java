08bee2fce45b480ce29d5e7cf3ea8e7d7ab0ed3d
eldar-kip <odzha.eldar.i.3.24@gmail.com>
2026-08-25 22:03:58 +0300

    добавил закрытие MainActivity

--- YchiCoran/app/src/main/java/com/example/ychicoran/MainActivity.java
+++ YchiCoran/app/src/main/java/com/example/ychicoran/MainActivity.java
@@ -123,6 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
-
+        finish();
     }
 }
